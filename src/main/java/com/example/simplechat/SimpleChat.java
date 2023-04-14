850cd44089c0814d8949e302920f24e5ce48c9f0
Rodrigo Moreira <rodrigo.moreira206@gmail.com>
2023-04-14 11:42:37 -0300

    Initial commit

---
+++ src/main/java/com/example/simplechat/SimpleChat.java
@@ -0,0 +1,85 @@
+package com.example.simplechat;
+
+import org.jgroups.*;
+import org.jgroups.util.Util;
+
+import java.io.*;
+import java.util.List;
+import java.util.LinkedList;
+
+public class SimpleChat extends ReceiverAdapter {
+    JChannel channel;
+    String user_name=System.getProperty("user.name", "n/a");
+    final List<String> state=new LinkedList<String>();
+
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        String line=msg.getSrc() + ": " + msg.getObject();
+        System.out.println(line);
+        synchronized(state) {
+            state.add(line);
+        }
+    }
+
+    public void getState(OutputStream output) throws Exception {
+        synchronized(state) {
+            Util.objectToStream(state, new DataOutputStream(output));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setState(InputStream input) throws Exception {
+        List<String> list=(List<String>)Util.objectFromStream(new DataInputStream(input));
+        synchronized(state) {
+            state.clear();
+            state.addAll(list);
+        }
+        System.out.println("received state (" + list.size() + " messages in chat history):");
+        for(String str: list) {
+            System.out.println(str);
+        }
+    }
+
+
+    private void start() throws Exception {
+        channel=new JChannel();
+        channel.setReceiver(this);
+        channel.connect("ChatCluster");
+        channel.getState(null, 10000);
+        eventLoop();
+        channel.close();
+    }
+
+    private void eventLoop() {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            try {
+
+                // Get a destination, <enter> means broadcast
+                Address destination = null;
+                System.out.print("Enter a destination: ");
+                System.out.flush();
+                String destinationName = in.readLine().toLowerCase();
+
+                System.out.print("> "); System.out.flush();
+                String line=in.readLine().toLowerCase();
+                if(line.startsWith("quit") || line.startsWith("exit")) {
+                    break;
+                }
+                line="[" + user_name + "] " + line;
+                Message msg = new Message(destination, line);
+                channel.send(msg);
+            }
+            catch(Exception e) {
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new SimpleChat().start();
+    }
+}
